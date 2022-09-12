c4f523294d8a464884606d4f68c87fc18982de76
muttaqinrizal <rizalm57@gmail.com>
2022-09-12 16:40:20 +0700

    unused syled

--- src/components/Header/styed.ts
+++ src/components/Header/styed.ts
@@ -10,15 +10,7 @@ export const Title = styled.h1`
   color: ${({ theme }) => theme.appTitleColor};
   font-size: 2.2rem;
 `;
-export const GithubLink = styled.a`
-  margin-left: 1rem;
-  svg {
-    fill: ${({ theme }) => theme.appTitleColor};
-  }
-  &:hover svg {
-    fill: #20546a;
-  }
-`;
+
 export const HeaderIconsContainer = styled.div`
   display: flex;
   align-items: center;
